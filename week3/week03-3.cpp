9e64de08155cb86bec7d0f6439de66ccc0a1926b
Hao940326 <a0903751284@gmail.com>
2024-03-08 11:42:33 +0800

    week03

---
+++ week3/week03-3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    while(a>0){
+        printf("a¬O%d\n",a);
+        a--;
+    }
+    printf("\n");
+    for(int b=5;b>0;b--){
+        printf("b¬O%d\n",b);
+    }
+    printf("\n");
+    for(int i=0;i<=5;i++){
+        printf("i¬O%d\n",i);
+    }
+}
