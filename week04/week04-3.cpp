e32e20ab2d40aee498433584de4cccbeb9051f8f
Hao940326 <a0903751284@gmail.com>
2024-03-15 11:08:04 +0800

    week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=9;i++){
+        cout<<"i¬O:"<<i<<endl;
+    }
+    cout<<endl;
+    for(int i=9;i>=1;i--){
+        cout<<"i¬O:"<<i<<endl;
+    }
+}
+
