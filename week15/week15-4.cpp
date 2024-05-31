405626a516142c410806838dd847ed7049790997
wei wao wang <88871132+Hao940326@users.noreply.github.com>
2024-05-31 11:34:08 +0800

    week15
    week15

---
+++ week15/week15-4.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    if('A'==65)printf("A==65\n");
+    printf("字母%c的值是:%d\n",65,'A');
+    printf("字母%c的值是:%d\n",66,'B');
+    printf("字母%c的值是:%d\n",67,'C');
+    printf("\n");
+    printf("字母%c的值是:%d\n",97,'a');
+}
